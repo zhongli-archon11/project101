8b0bbdb6412ce651cd73ba34bfae7548c7d5b5c1
Rex Lapis <91016552+zhongli-archon11@users.noreply.github.com>
2022-07-23 21:10:32 +0530

    Logout

---
+++ logout.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// remove all session variables
+    session_unset(); 
+
+// destroy the session 
+    session_destroy(); 
+
+    header('location:loginform.php')
+?>
+
+</body>
+</html> 
